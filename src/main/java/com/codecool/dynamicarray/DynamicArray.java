f93fdc8e1df8231cb5d25bc3152189ac66acc838
appuser <appuser@journey2-748b9dc9bc-ldx7d>
2020-12-06 11:00:25 +0000

    starter project

---
+++ src/main/java/com/codecool/dynamicarray/DynamicArray.java
@@ -0,0 +1,32 @@
+package com.codecool.dynamicarray;
+
+public class DynamicArray {
+    private int[] array;
+
+    public DynamicArray(int capacity) {
+    }
+
+    public DynamicArray() {
+    }
+
+    public int getCapacity() {
+        return 0;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void add(int value) {
+    }
+
+    public int get(int index) {
+        return 0;
+    }
+
+    public void remove(int indexToBeRemoved) {
+    }
+
+    public void insert(int index, int newValue) {
+    }
+}
