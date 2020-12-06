c6723154522e44bb434e41b00b799cfa89a61b7c
pusztaizoltan <a.pusztaizoltan@gmail.com>
2020-12-06 15:33:01 +0100

    add complete except exceptions;

--- src/main/java/com/codecool/dynamicarray/DynamicArray.java
+++ src/main/java/com/codecool/dynamicarray/DynamicArray.java
@@ -1,32 +1,108 @@
 package com.codecool.dynamicarray;
 
+import java.util.Arrays;
+
 public class DynamicArray {
-    private int[] array;
+    private int capacity;
+    public int[] array;
+//    private int[] array;
+    private int size = 0;
+
+    // to test in preset array use array with length mor then 4
+    public DynamicArray(int[] testArray) {
+        this.array = testArray;
+        this.capacity = testArray.length;
+        this.size = testArray.length;
+    }
 
     public DynamicArray(int capacity) {
+        this.array = new int[capacity];
+        this.capacity = capacity;
     }
 
     public DynamicArray() {
+        this.array = new int[4];
+        this.capacity = 4;
     }
 
     public int getCapacity() {
-        return 0;
+        return capacity;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public void add(int value) {
+        if(size == capacity) {
+            capacity *= 2;
+            array = Arrays.copyOf(array, capacity);
+
+        }
+        array[size] = value;
+        size++;
     }
 
-    public int get(int index) {
-        return 0;
+    public int get(int index){
+        return array[index];
     }
+
+//    public int get(int index) throws ArrayIndexOutOfBoundEception {
+//        if (size <= index){ throw new ArrayIndexOutOfBoundEception();}
+//        return array[index];
+//
+//    }
+
 
     public void remove(int indexToBeRemoved) {
+        int[] newArray = new int[array.length - 1];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (i == indexToBeRemoved) continue;
+            newArray[j] = array[i];
+            j +=1;
+        }
+        array = newArray;
+        size--;
     }
 
+
     public void insert(int index, int newValue) {
+        if(size == capacity) {
+            capacity *= 2;
+            array = Arrays.copyOf(array, capacity);
+//            System.out.println("double capaciti");
+        }
+        if(index >=size) {
+            add(newValue);
+//            System.out.println("add to the end");
+        } else {
+            int[] newArray = new int[capacity];
+            for (int i = 0; i < index; i++) {
+                newArray[i] = array[i];
+            }
+            newArray[index] = newValue;
+            for (int i = index; i < size; i++) {
+                newArray[i+1] = array[i];
+            }
+            array = newArray;
+            size++;
+        }
     }
+    public String toString() {
+        String stingReprezentation = "[";
+        if (0 == size) return stingReprezentation + "]";
+        for(int i = 0; i<size; i++) {
+            stingReprezentation += array[i];
+            if (i < size - 1) {
+                stingReprezentation += ", ";
+            } else {
+                stingReprezentation += "]";}
+        }
+        return stingReprezentation;
+    }
+
+//    private class ArrayIndexOutOfBoundEception extends Exception {
+//    }
+
+
 }
--- src/main/java/com/codecool/dynamicarray/Main.java
+++ src/main/java/com/codecool/dynamicarray/Main.java
@@ -1,7 +1,20 @@
 package com.codecool.dynamicarray;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Execute all tests to check your work.");
+        int[] testArray = {1, 2, 3, 4, 5};
+        DynamicArray array = new DynamicArray(testArray);
+        System.out.println("size" + array.size());
+        System.out.println("capcity" + array.getCapacity());
+        array.insert(2, 10);
+        Arrays.stream(array.array).forEach(i -> System.out.println(i +" "));
+        System.out.println("size" + array.size());
+        System.out.println("capcity" + array.getCapacity());
+
+
     }
+
 }
